c29382c02ba5fbf84c77b8fc5c729090bca366e7
Administrator <luisfa-bi9@hotmail.com>
2019-08-29 00:06:43 -0500

    terminando

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,14 @@
+import { PeriodicElement } from './model/home';
 import { Component, OnInit } from '@angular/core';
 
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material/table';
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Monday 10 2:26 pm', street: 'VR sad as', icon: 'fa fa-truck', price: '$250.0', quantity: 1},
+
+];
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,6 +16,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  displayedColumns: string[] = ['select', 'day', 'street', 'icon', 'price', 'quantity'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/home/model/home.ts
@@ -0,0 +1,9 @@
+export interface PeriodicElement {
+
+  name: string;
+  position: number;
+  street: string;
+  icon: string;
+  price: string;
+  quantity: number;
+}
